0f538abf7034358b24dc11df6335e4099e66400c
karimzade_k <karimzade_k@STHQ0122-01>
2026-03-01 13:18:17 +0400

    BackgroundService classi ile numune yazildi

---
+++ JwtWithIdentity/BackgroundServices/SomeBGService.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+namespace JwtWithIdentity.BackgroundServices;
+
+public class SomeBGService : BackgroundService
+{
+
+    // ////////////////////////////////////////////////////////////////
+
+    // // Simple Version
+
+    // protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    // {
+    //     Console.WriteLine("SomeBGService started...");
+    // 
+    //     return Task.CompletedTask;
+    // }
+
+    // ////////////////////////////////////////////////////////////////
+    
+    // ////////////////////////////////////////////////////////////////
+
+    // // TIMER Example 
+
+    private Timer _timer;
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine("Execute ise baslayir");
+        _timer = new Timer(Run, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+        return Task.CompletedTask;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("BG service ise basladi...");
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Bg service isini yekunlasdirdi...");
+        return base.StopAsync(cancellationToken);
+    }
+
+    private void Run(object? obj)
+    {
+        Console.WriteLine("Hakuna Matata");
+    }
+
+    ////////////////////////////////////////////////////////////////
+
+}
--- JwtWithIdentity/Program.cs
+++ JwtWithIdentity/Program.cs
@@ -17,7 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // builder.Services.AddHostedService<MyBGService>();
-
+builder.Services.AddHostedService<SomeBGService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
